6ed176b753c551e0dc398048f09db942c83abd0b
N/A <ruben.wood1@gmail.com>
2019-09-03 14:17:16 +0100

    Initial Upload
    Initial upload of Sorted test, includes skeleton for checkout  class and Item class and test class (Program.cs)

---
+++ SortedTest/Checkout.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortedTest
+{
+    class Checkout
+    { 
+        // Used to scan an item
+        public void Scan(Item item)
+        {
+
+        }
+
+        // calculates and returns total price
+        public decimal Total()
+        {
+            return 0;
+        }
+    }
+
+    public class Item
+    {
+        // Item properties
+        public string SKU; // item ID
+        public decimal UnitPrice = 0; // individual item price (no offers)
+
+        // properties if the item is on offer
+        public bool hasOffer = false;
+        public int offerAmnt = 0; // amount of this item required to trigger offer
+        public decimal offerPice = 0; // cost of items on offer
+
+        //Item constructor, requires and SKU and a price
+        public Item(string inSKU, decimal inPrice)
+        {
+            SKU = inSKU;
+            UnitPrice = inPrice;
+        }
+
+        // public function that adds an offer to this instance of Item
+        // expects an int (amnt), determines how many of this item are required to trigger offer
+        // expects a deciaml (price), determines the price of the offer
+        public void addOffer(int amnt, decimal price)
+        {
+            this.hasOffer = true;
+            this.offerAmnt = amnt;
+            this.offerPice = price;
+        }
+
+    }
+}
---
+++ SortedTest/Program.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortedTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /// Here is where all of the tests will go
+            /// first create an instance of and object and log its properties to make sure its working
+            
+
+
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
