e3aab7bba714389b1289c11515b106ab3833d0ef
N/A <ruben.wood1@gmail.com>
2019-09-03 14:22:20 +0100

    Test Item class
    added a simple test to ensure item class is working

--- SortedTest/Program.cs
+++ SortedTest/Program.cs
@@ -12,10 +12,17 @@ namespace SortedTest
         {
             /// Here is where all of the tests will go
             /// first create an instance of and object and log its properties to make sure its working
-            
+            Item apple = new Item("A99", 0.50m);
+            Item bisc = new Item("B15", 0.30m);
+            Item crumpet = new Item("C40", 0.60m);
 
+            apple.addOffer(3, 1.30m);
+            bisc.addOffer(2, 0.45m);
 
-            Console.WriteLine("Hello World!");
+            /// make sure the Item class has been establilshed, log out some info belonging to an Item
+            Console.WriteLine(apple.SKU);
+            Console.WriteLine(apple.UnitPrice);
+
             Console.ReadKey();
         }
     }
